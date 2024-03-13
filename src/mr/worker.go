4a5c26b8dc2f278512bb79a63d3d42f948c817dc
yingshaowen <yingshaowen@xiaomi.com>
2024-03-13 20:29:24 +0800

    tmp

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -10,7 +10,6 @@ import (
 	"net/rpc"
 	"os"
 	"path/filepath"
-	"sync"
 	"time"
 )
 
@@ -26,6 +25,220 @@ func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
+}
+
+type MapWorker struct {
+	nReduce int
+	wd      string
+	f       func(string, string) []KeyValue
+	tasks   chan *AssignMapTaskReply
+	done    chan struct{}
+}
+
+func (m *MapWorker) Request() {
+	c, err := rpc.DialHTTP("unix", coordinatorSock())
+	if err != nil {
+		log.Fatalln("[MapWorker]:", err)
+	}
+	defer c.Close()
+
+	for {
+		allMapTasksDoneReply := &AllMapTasksDoneReply{}
+		if err := c.Call("Coordinator.AllMapTasksDone", &AllMapTasksDoneArgs{}, allMapTasksDoneReply); err != nil ||
+			allMapTasksDoneReply.Done {
+			close(m.tasks)
+			return
+		}
+
+		assignMapTaskReply := &AssignMapTaskReply{}
+		if err := c.Call("Coordinator.AssignMapTask", &AssignMapTaskArgs{}, assignMapTaskReply); err != nil {
+			time.Sleep(time.Second)
+		} else {
+			m.tasks <- assignMapTaskReply
+		}
+	}
+}
+
+func (m *MapWorker) Do() {
+	for task := range m.tasks {
+		m.do(task)
+	}
+}
+
+func (m *MapWorker) Done() <-chan struct{} {
+	return m.done
+}
+
+func (m *MapWorker) do(task *AssignMapTaskReply) {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("[MapWorker]:", err)
+		}
+	}()
+
+	log.Println("[MapWorker]: assigned to map task", task.TaskId)
+
+	fin, err := os.Open(task.Filename)
+	if err != nil {
+		panic(err)
+	}
+	defer fin.Close()
+	content, err := io.ReadAll(bufio.NewReader(fin))
+	if err != nil {
+		panic(err)
+	}
+
+	kvs := m.f(task.Filename, string(content))
+
+	type info struct {
+		enc    *json.Encoder
+		name   string
+		rename string
+	}
+	infos := make([]*info, m.nReduce)
+
+	for _, kv := range kvs {
+		i := ihash(kv.Key) % m.nReduce
+		if infos[i] == nil {
+			fout, err := os.CreateTemp(m.wd, "map-temp*")
+			if err != nil {
+				panic(err)
+			}
+			defer func() {
+				fout.Close()
+				os.Remove(fout.Name())
+			}()
+
+			infos[i] = &info{json.NewEncoder(fout), fout.Name(), filepath.Join(m.wd, fmt.Sprintf("mr-%d-%d", task.TaskId, i))}
+		}
+
+		if err := infos[i].enc.Encode(&kv); err != nil {
+			panic(err)
+		}
+	}
+
+	intermediate := make(map[int]string, m.nReduce)
+	for i, v := range infos {
+		if v == nil {
+			continue
+		}
+		if err := os.Rename(v.name, v.rename); err != nil {
+			panic(err)
+		} else {
+			intermediate[i] = v.rename
+		}
+	}
+
+	go func() {
+		if err := call("Coordinator.NotifyOneMapTaskDone", &NotifyOneMapTaskDoneArgs{task.TaskId, intermediate}, &NotifyOneMapTaskDoneReply{}); err != nil {
+			log.Println("[MapWorker]:", err)
+		}
+	}()
+}
+
+type ReduceWorker struct {
+	nReduce int
+	wd      string
+	f       func(string, []string) string
+	tasks   chan *AssignReduceTaskReply
+	done    chan struct{}
+}
+
+func (r *ReduceWorker) Request() {
+	c, err := rpc.DialHTTP("unix", coordinatorSock())
+	if err != nil {
+		log.Fatalln("[ReduceWorker]:", err)
+	}
+	defer c.Close()
+
+	for {
+		allReduceTasksDoneReply := &AllReduceTasksDoneReply{}
+		if err := call("Coordinator.AllReduceTasksDone", &AllReduceTasksDoneArgs{}, allReduceTasksDoneReply); err != nil ||
+			allReduceTasksDoneReply.Done {
+			close(r.tasks)
+			return
+		}
+
+		assignReduceTaskReply := &AssignReduceTaskReply{}
+		if err := call("Coordinator.AssignReduceTask", &AssignReduceTaskArgs{}, assignReduceTaskReply); err != nil {
+			time.Sleep(time.Second)
+		} else {
+			r.tasks <- assignReduceTaskReply
+		}
+	}
+}
+
+func (r *ReduceWorker) Do() {
+	for task := range r.tasks {
+		r.do(task)
+	}
+}
+
+func (r *ReduceWorker) Done() <-chan struct{} {
+	return r.done
+}
+
+func (r *ReduceWorker) do(task *AssignReduceTaskReply) {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("[ReduceWorker]:", err)
+		}
+	}()
+
+	log.Println("[ReduceWorker]: assigned to reduce task", task.TaskId)
+
+	fout, err := os.CreateTemp(r.wd, "reduce-temp*")
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		fout.Close()
+		os.Remove(fout.Name())
+	}()
+	bw := bufio.NewWriter(fout)
+
+	mp := make(map[string][]string)
+	for _, file := range task.Files {
+		fin, err := os.Open(file)
+		if err != nil {
+			panic(err)
+		}
+		defer fin.Close()
+
+		dec := json.NewDecoder(bufio.NewReader(fin))
+		for {
+			var kv KeyValue
+			if err := dec.Decode(&kv); err != nil {
+				if err != io.EOF {
+					panic(err)
+				} else {
+					break
+				}
+			}
+			k := kv.Key
+			mp[k] = append(mp[k], kv.Value)
+		}
+	}
+
+	for k, v := range mp {
+		if _, err := fmt.Fprintf(bw, "%s %s\n", k, r.f(k, v)); err != nil {
+			panic(err)
+		}
+	}
+
+	if err := bw.Flush(); err != nil {
+		panic(err)
+	}
+
+	if err := os.Rename(fout.Name(), fmt.Sprintf("mr-out-%d", task.TaskId)); err != nil {
+		panic(err)
+	}
+
+	go func() {
+		if err := call("Coordinator.NotifyOneReduceTaskDone", &NotifyOneReduceTaskDoneArgs{task.TaskId}, &NotifyOneReduceTaskDoneReply{}); err != nil {
+			log.Println("[ReduceWorker]:", err)
+		}
+	}()
 }
 
 // main/mrworker.go calls this function.
@@ -45,184 +258,9 @@ func Worker(mapf func(string, string) []KeyValue, reducef func(string, []string)
 	}
 	nReduce := getnReduceReply.NReduce
 
-	var wg sync.WaitGroup
-
 	// Map
-	for i := 0; i < 5; i++ {
-		wg.Add(1)
-		go func(workerId int) {
-			log.Printf("[Map worker %d]: started\n", workerId)
-			defer wg.Done()
-
-			for {
-				allMapTasksDoneReply := &AllMapTasksDoneReply{}
-				if err := call("Coordinator.AllMapTasksDone", &AllMapTasksDoneArgs{}, allMapTasksDoneReply); err != nil ||
-					allMapTasksDoneReply.Done {
-					log.Printf("[Map worker %d]: exit\n", workerId)
-					return
-				}
-
-				assignMapTaskReply := &AssignMapTaskReply{}
-				if err := call("Coordinator.AssignMapTask", &AssignMapTaskArgs{}, assignMapTaskReply); err != nil {
-					log.Printf("[Map worker %d]: %s\n", workerId, err)
-					time.Sleep(time.Second)
-					continue
-				}
-				log.Printf("[Map worker %d]: assigned to map task %d\n", workerId, assignMapTaskReply.TaskId)
-
-				go func() {
-					defer func() {
-						if err := recover(); err != nil {
-							log.Printf("[Map worker %d]: %s\n", workerId, err)
-						}
-					}()
-
-					fin, err := os.Open(assignMapTaskReply.Filename)
-					if err != nil {
-						panic(err)
-					}
-					defer fin.Close()
-					content, err := io.ReadAll(bufio.NewReader(fin))
-					if err != nil {
-						panic(err)
-					}
-
-					kvs := mapf(assignMapTaskReply.Filename, string(content))
-
-					type info struct {
-						enc    *json.Encoder
-						name   string
-						rename string
-					}
-					infos := make([]*info, nReduce)
-
-					for _, kv := range kvs {
-						i := ihash(kv.Key) % nReduce
-						if infos[i] == nil {
-							fout, err := os.CreateTemp(wd, "map-temp*")
-							if err != nil {
-								panic(err)
-							}
-							defer func() {
-								fout.Close()
-								os.Remove(fout.Name())
-							}()
-
-							infos[i] = &info{json.NewEncoder(fout), fout.Name(), filepath.Join(wd, fmt.Sprintf("mr-%d-%d", assignMapTaskReply.TaskId, i))}
-						}
-
-						if err := infos[i].enc.Encode(&kv); err != nil {
-							panic(err)
-						}
-					}
-
-					intermediate := make(map[int]string, nReduce)
-					for i, v := range infos {
-						if v == nil {
-							continue
-						}
-						if err := os.Rename(v.name, v.rename); err != nil {
-							panic(err)
-						} else {
-							intermediate[i] = v.rename
-						}
-					}
-
-					if err := call("Coordinator.NotifyOneMapTaskDone", &NotifyOneMapTaskDoneArgs{assignMapTaskReply.TaskId, intermediate}, &NotifyOneMapTaskDoneReply{}); err != nil {
-						panic(err)
-					}
-				}()
-			}
-		}(i)
-	}
-	wg.Wait()
 
 	// Reduce
-	for i := 0; i < 5; i++ {
-		wg.Add(1)
-
-		go func(workerId int) {
-			log.Printf("[Reduce worker %d]: started\n", workerId)
-			defer wg.Done()
-
-			for {
-				allReduceTasksDoneReply := &AllReduceTasksDoneReply{}
-				if err := call("Coordinator.AllReduceTasksDone", &AllReduceTasksDoneArgs{}, allReduceTasksDoneReply); err != nil ||
-					allReduceTasksDoneReply.Done {
-					log.Printf("[Reduce worker %d]: exit\n", workerId)
-					return
-				}
-
-				assignReduceTaskReply := &AssignReduceTaskReply{}
-				if err := call("Coordinator.AssignReduceTask", &AssignReduceTaskArgs{}, assignReduceTaskReply); err != nil {
-					log.Printf("[Reduce worker %d]: %s\n", workerId, err)
-					time.Sleep(time.Second)
-					continue
-				}
-				log.Printf("[Reduce worker %d]: assigned to reduce task %d\n", workerId, assignReduceTaskReply.TaskId)
-
-				go func() {
-					defer func() {
-						if err := recover(); err != nil {
-							log.Printf("[Reduce worker %d]: %s\n", workerId, err)
-						}
-					}()
-
-					fout, err := os.CreateTemp(wd, "reduce-temp*")
-					if err != nil {
-						panic(err)
-					}
-					defer func() {
-						fout.Close()
-						os.Remove(fout.Name())
-					}()
-					bw := bufio.NewWriter(fout)
-
-					mp := make(map[string][]string)
-					for _, file := range assignReduceTaskReply.Files {
-						fin, err := os.Open(file)
-						if err != nil {
-							panic(err)
-						}
-						defer fin.Close()
-
-						dec := json.NewDecoder(bufio.NewReader(fin))
-						for {
-							var kv KeyValue
-							if err := dec.Decode(&kv); err != nil {
-								if err != io.EOF {
-									panic(err)
-								} else {
-									break
-								}
-							}
-							k := kv.Key
-							mp[k] = append(mp[k], kv.Value)
-						}
-					}
-
-					for k, v := range mp {
-						if _, err := fmt.Fprintf(bw, "%s %s\n", k, reducef(k, v)); err != nil {
-							panic(err)
-						}
-					}
-
-					if err := bw.Flush(); err != nil {
-						panic(err)
-					}
-
-					if err := os.Rename(fout.Name(), fmt.Sprintf("mr-out-%d", assignReduceTaskReply.TaskId)); err != nil {
-						panic(err)
-					}
-
-					if err := call("Coordinator.NotifyOneReduceTaskDone", &NotifyOneReduceTaskDoneArgs{assignReduceTaskReply.TaskId}, &NotifyOneReduceTaskDoneReply{}); err != nil {
-						panic(err)
-					}
-				}()
-			}
-		}(i)
-	}
-	wg.Wait()
 
 	if err := call("Coordinator.Shutdown", &ShutdownArgs{}, &ShutdownReply{}); err != nil {
 		log.Println("[Worker]:", err)
@@ -234,7 +272,6 @@ func call(serviceMethod string, args any, reply any) error {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	c, err := rpc.DialHTTP("unix", coordinatorSock())
 	if err != nil {
-		log.Println("dialing:", err)
 		return err
 	}
 	defer c.Close()
